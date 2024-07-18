fcaba99f29860c902352c31365932be590d7b612
Windows 10 <mustagheesabbasi@gmail.com>
2024-07-18 20:46:54 +0500

    Recipe Secrets

---
+++ app/src/main/java/com/example/Recipeapp/MainActivity.java
@@ -0,0 +1,58 @@
+/* Assignment: 3
+Campus: Ashdod
+Authors:
+Eliran Naduyev 312089105
+Maria Garber
+*/
+
+package com.example.Recipeapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFrag()).commit();
+    }
+
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    selectedFragment = new HomeFrag();
+                } else if (itemId == R.id.nav_favorites) {
+                    selectedFragment = new FavoriteFrag();
+                } else if (itemId == R.id.nav_search) {
+                    selectedFragment = new SearchrecipeActivity();
+                } else {
+                    return false;
+                }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+}
